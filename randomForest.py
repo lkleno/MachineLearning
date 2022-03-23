aa7634ff5dd94cf224b70ca27a9508d0dbe2cca5
lkleno <92755216+lkleno@users.noreply.github.com>
2022-03-23 10:47:33 +0100

    Add files via upload

--- randomForest.py
+++ randomForest.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("KopijaKopijaPodatki_sem_v6_NH.csv", delimiter=';')
+df = pd.read_csv("KopijaKopijaPodatki_sem_v5_NH.csv", delimiter=';')
 
 inputs = df.drop(['Odvzeta količina'],axis = 'columns')
 target = df['Odvzeta količina']
@@ -25,6 +25,12 @@ test = x_valid['Kosovnica']
 x_valid = x_valid.drop(['Kosovnica'],axis='columns')
 x_train = x_train.drop(['Kosovnica'],axis='columns')
 
+
+fn = list()
+for col in inputs_n.columns:
+    fn.append(col)
+
+print(fn)
 
 from sklearn.ensemble import RandomForestRegressor
 
@@ -79,3 +85,39 @@ mape = np.mean(np.abs((gt-pred)/np.abs(gt)))
 print(mape)
 print('Mean Absolute Percentage Error (MAPE):', round(mape * 100, 2))
 print('Accuracy:', round(100*(1 - mape), 2))
+
+import matplotlib.pyplot as plt
+'''RFPred = model.predict(x_valid)
+plt.scatter(test[:100],RFPred[:100], marker='o',color='#F43CF4',label='Random forest')
+plt.scatter(test[:100],y_valid[:100],color='r',marker='o',label='Actual value')
+plt.xlabel("Unique ID")
+plt.ylabel("Order quantity")
+plt.legend()
+plt.show()'''
+
+
+import time
+import numpy as np
+print()
+print()
+print()
+print()
+print()
+print()
+start_time = time.time()
+importances = model.feature_importances_
+print(importances)
+std = np.std([tree.feature_importances_ for tree in model.estimators_], axis=0)
+elapsed_time = time.time() - start_time
+
+print(f"Elapsed time to compute the importances: {elapsed_time:.3f} seconds")
+import pandas as pd
+
+#forest_importances = pd.Series(importances, index=fn)
+
+fig, ax = plt.subplots()
+#forest_importances.plot.bar(yerr=std, ax=ax)
+plt.scatter(importances,fn, marker='o',color='#F43CF4',label='Random forest')
+ax.set_title("Feature importances using MDI")
+ax.set_ylabel("Mean decrease in impurity")
+fig.tight_layout()
